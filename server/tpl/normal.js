8760b552483dc65a387378e3ca2bd50b51c0fcfa
ianl <ianl@coreplus.com.au>
2018-09-03 22:51:21 +1000

    add html template

---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta charset="viewport" content="width=device-width, initial-scale=1">
+    <title>Koa Server HTML</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+      <div class="col-md-8">
+        <h1>Hi Luke</h1>
+        <p>This is Ian</p>
+      </div>
+      <div class="col-md-4">
+        <p>Test html page</p>
+      </div>
+      </div>
+    </div>
+  </body>
+</html>
+`;
